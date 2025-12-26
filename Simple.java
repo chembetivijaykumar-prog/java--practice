8a4eb42226092f769a67f08141b55a42cb90b0bb
vijay <chembetivijaykumar@gmail.com>
2025-12-26 16:59:34 +0530

    All My coding practice

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 public class Simple {
     public static void main(String[] args) {
         System.out.println("welcome back to java preparation");
-        int a[] = {21, 21, 45, 21, 50, 49, 50};
+        int a[] = {7, 21, 45, 20, 50, 49, 50};
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] == a[j]) {
